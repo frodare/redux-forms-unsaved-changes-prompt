6fc4ab89f84e5b6c3a5e62608a689aab562ad26d
Charles Howard <cdhoward@add123.com>
2018-09-27 14:04:44 -0400

    https://github.com/frodare/redux-forms-unsaved-changes-prompt/issues/1

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Field, reduxForm } from 'redux-form'
+import { Field, reduxForm, formValueSelector } from 'redux-form'
 import unsavedPrompt from './unsavedChangesPrompt'
 import { compose } from 'recompose'
+import { connect } from 'react-redux'
 
 export const NAME = 'contact'
 
 let ContactForm = props => {
   const { handleSubmit } = props
+  console.log(props)
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={props.dirty ? 'dirty' : ''}>
+      <h2>{props.name}</h2>
       <div>
         <label htmlFor='firstName'>First Name</label>
         <Field name='firstName' component='input' type='text' />
@@ -26,7 +29,18 @@ let ContactForm = props => {
   )
 }
 
+const fieldSelector = formValueSelector(NAME)
+
+const mapStateToProps = state => ({
+  initialValues: {
+    firstName: 'John',
+    lastName: 'Snow'
+  },
+  name: fieldSelector(state, 'firstName') + ' ' + fieldSelector(state, 'lastName')
+})
+
 const enhance = compose(
+  connect(mapStateToProps),
   reduxForm({form: NAME}),
   unsavedPrompt(NAME, 'Some message here!')
 )
